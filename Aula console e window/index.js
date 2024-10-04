cb87a0ce5ac423ef6e221e3169ce96c0a41d52e1
joao <joaosouza.uninove@gmail.com>
2024-10-04 16:30:06 -0300

    primeiro commit

---
+++ Aula console e window/index.js
@@ -0,0 +1,16 @@
+let nome = "João"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+//window.alert(nome)
+// Gerra uma sinal de alerta no console na variavel inserida
+
+//nome = window.prompt("Insira seu nome:")
+//Pode ser usado para colocar uma string na caixa de alerta, mas precisa definir uma variavel
+
+//let confirmacao = window.confirm("Confirma esta ação?")
+//Serve para confirmar sim ou não, no console aparece como true e false 
+
+console.log(confirmacao)
---
+++ exercicio1/index.js
@@ -0,0 +1,14 @@
+const primeiroNome = prompt ("Informe o Primeiro Nome:")
+const sobrenome = prompt ("Informe o Sobrenome:")
+const campoDeEstudo = prompt ("Informe o Campo de Estudos:")
+const AnoDeNascimento = prompt ("Informe o Ano de Nascimento:")
+
+alert (
+  "Cadastrado com sucesso!" +
+  "\n Nome Completo:" + primeiroNome + " " + sobrenome +
+  "\n Campo de Estudos:" + campoDeEstudo +
+  "\n Idade:" + (2024 - AnoDeNascimento)
+)
+
+
+ 
